94dbc8c7625db3cbf69d31688ad87789dcc3cf9a
Guilleme <guilleme.benoit@toolib.fr>
2023-01-26 19:04:22 +0100

    MVP

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import crud.storyCrud as storyCrud
-import crud.epicCrud as epicCrud
-import crud.workflowCrud as workflowCrud
 import story_parser as parser
 from dotenv import load_dotenv
 import os
@@ -8,43 +6,43 @@ import os
 load_dotenv()
 
 url = os.getenv('SHORTCUT_API_URL')
-token = os.getenv('SHORTCUT_ACCESS_TOKEN')
 workflow_name = os.getenv('SHORTCUT_WORKFLOW')
 
 global_headers = {
     'content-type': 'application/json',
-    'Shortcut-Token': token
+    'Shortcut-Token': os.getenv('SHORTCUT_ACCESS_TOKEN')
 }
 
 
 def main():
     stories = parser.parse_csv(os.getenv('CSV_PATH'))
+    handle_adding_stories(stories)
+    handle_adding_block_relation(stories)
+
+
+def handle_adding_stories(stories: list):
     for story in stories:
+        # Getting epic id or creating it
         storyCrud.match_epic(url, global_headers, story)
-
-        match_state(url, global_headers, story, workflow_name)
-
-        print(story)
-
+        # Getting state id
+        storyCrud.match_state(url, global_headers, story, workflow_name)
         # Adding stories
-        storyCrud.add_story(
-            url, global_headers, story["description"], story["state_id"], story["epic_id"], story["blocked by"])
+        # Storing the id, will be convenient for handling the blocking
+        story["id"] = storyCrud.add_story(
+            url, global_headers, story["description"], story["state_id"], story["epic_id"])["id"]
 
 
-
-
-
-
-def match_state(url: str, global_headers: dict, story: dict, workflow_name: str):
-    workflow = workflowCrud.get_workflow_by_name(url, global_headers, workflow_name)
-    for state in workflow['states']:
-        print(state['name'])
-        print(story['status'])
-        if(story['status']==state['name']):
-            story["state_id"] = state['id']
-            return story
-        # Note that if the status doesn't exist it won't create the attribute and thus crash later
-    
+def handle_adding_block_relation(stories: list):
+    # Handling blocked_by
+    # we're relooping so the stories can be put in any order in the CSV file
+    # if we wouldn't reloop, we couldn't refer to a story before creating it
+    # i just don't think we should be dependant from the order of the stories in the CSV
+    for story in stories:
+        if(story['blocked by']):
+            story2 = storyCrud.get_story_by_name(
+                url, global_headers, story['blocked by'])
+            rel = storyCrud.create_relation(
+                url, global_headers, story['id'], story2['id'], "blocks")
 
 
 if __name__ == "__main__":
